2ff47e7a79506a610987a2c5dcb3dcca3025e61d
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-24 14:13:39 +0200

    modifies the Validator service

--- src/main/java/com/banking/Main.java
+++ src/main/java/com/banking/Main.java
@@ -2,7 +2,6 @@ package com.banking;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 
 }
--- src/main/java/com/banking/service/DataAuthenticationStorage.java
+++ src/main/java/com/banking/service/DataAuthenticationStorage.java
@@ -1,4 +1,5 @@
 package com.banking.service;
 
 public class DataAuthenticationStorage {
+
 }
